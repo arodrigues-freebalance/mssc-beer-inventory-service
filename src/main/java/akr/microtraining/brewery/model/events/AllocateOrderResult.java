5e5475f69509b25d7811946b596edb9b51628068
arodrigues_freebalance <arodrigues@freebalance.com>
2020-07-10 12:45:17 +0100

    Fix de-serialization issues. Closes #15

--- src/main/java/akr/microtraining/brewery/model/events/AllocateOrderResult.java
+++ src/main/java/akr/microtraining/brewery/model/events/AllocateOrderResult.java
@@ -1,5 +1,7 @@
 package akr.microtraining.brewery.model.events;
 
+import java.io.Serializable;
+
 import akr.microtraining.brewery.model.BeerOrderDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,7 +12,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class AllocateOrderResult {
+public class AllocateOrderResult implements Serializable {
+	
+	private static final long serialVersionUID = -8871393604664868884L;
+	
     private BeerOrderDto beerOrderDto;
     private Boolean allocationError = false;
     private Boolean pendingInventory = false;
